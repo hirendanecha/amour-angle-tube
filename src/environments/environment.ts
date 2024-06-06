5c854c8dcd1b845ea1fb5662398e49c1aea9ef07
vatsal <vso.opash@gmail.com>
2024-06-06 11:38:57 +0530

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+const loginUrl = 'https://amourangles.tube/login';
+const logoutUrl = 'https://amourangles.tube/logout';
+const frontendUrl = 'https://tube.amourangles.tube/';
+const backendUrl =  'https://api.amourangles.tube/';
+
+// const frontendUrl = 'http://localhost:4200/';
+// const backendUrl = 'http://localhost:8080/';
+// const loginUrl = 'http://localhost:4200/login';
+const wasabiUrl = 'https://organic-dating.s3.wasabisys.com/'
+
+
+export const environment = {
+  production: false,
+  frontendUrl: frontendUrl,
+  backendUrl: backendUrl,
+  loginUrl: loginUrl,
+  apiUrl: `${backendUrl}api/v1/`,
+  domain: '.amourangles.tube',
+  wasabiUrl: wasabiUrl,
+  socketUrl: `${backendUrl}`,
+  conferenceUrl: 'https://facetime.tube/',
+  logoutUrl: logoutUrl
+
+};
+
